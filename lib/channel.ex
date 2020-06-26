d3bbfccb5fbbbcd81576d7f7e13594fc6628d158
Roig <groig@protonmail.com>
2020-06-26 00:55:32 -0400

    add propper pubsub

--- lib/channel.ex
+++ lib/channel.ex
@@ -1,18 +1,14 @@
 defmodule Channel do
   def make do
-    spawn(&Channel.loop/0)
+    spawn(fn -> Channel.loop([]) end)
   end
 
   def write(channel, val) do
     send(channel, {:write, val, self()})
 
     receive do
-      {:writed, _channel, val} -> val
+      {:written, _channel, val} -> val
     end
-  end
-
-  def pub(channel, val) do
-    send(channel, {:write, val, self()})
   end
 
   def read(channel) do
@@ -23,23 +19,43 @@ defmodule Channel do
     end
   end
 
-  def loop do
+  def pub(channel, val) do
+    send(channel, {:pub, val})
+  end
+
+  def sub(channel) do
+    send(channel, {:sub, self()})
+
+    receive do
+      {:pub, _channel, val} -> val
+    end
+  end
+
+  def loop(subs) do
     receive do
       {:read, read_caller} ->
         receive do
           {:write, val, write_caller} ->
             send(read_caller, {:read, self(), val})
-            send(write_caller, {:writed, self(), val})
-            loop()
+            send(write_caller, {:written, self(), val})
+            loop(subs)
         end
 
       {:write, val, write_caller} ->
         receive do
           {:read, read_caller} ->
             send(read_caller, {:read, self(), val})
-            send(write_caller, {:writed, self(), val})
-            loop()
+            send(write_caller, {:written, self(), val})
+            loop(subs)
         end
+
+      {:sub, sub_caller} ->
+        loop(subs ++ [sub_caller])
+
+      {:pub, val} ->
+        subs |> Enum.each(fn sub -> Task.start(fn -> send(sub, {:pub, self(), val}) end) end)
+
+        loop([])
     end
   end
 end
--- lib/hub.ex
+++ lib/hub.ex
@@ -8,11 +8,9 @@ defmodule Hub do
     send_resp(conn, 200, "Send channel id")
   end
 
-  get "/:channel_id" do
-    get_data(conn, channel_id)
-  end
+  # Default
 
-  get "/pubsub/:channel_id" do
+  get "/:channel_id" do
     get_data(conn, channel_id)
   end
 
@@ -20,16 +18,27 @@ defmodule Hub do
     post_data(conn, channel_id)
   end
 
-  match "/pubsub/:channel_id", via: [:post, :patch, :put] do
-    pub_data(conn, channel_id)
-  end
-
   delete "/:channel_id" do
     delete_channel(conn, channel_id)
   end
 
+  # PubSub
+
+  get "/pubsub/:channel_id" do
+    sub_data(conn, channel_id)
+  end
+
+  match "/pubsub/:channel_id", via: [:post, :patch, :put] do
+    pub_data(conn, channel_id)
+  end
+
   delete "/pubsub/:channel_id" do
     delete_channel(conn, channel_id)
+  end
+
+  def init(options) do
+    # initialize options
+    options
   end
 
   defp get_data(conn, channel_id) do
@@ -44,10 +53,6 @@ defmodule Hub do
     send_resp(conn, 200, data)
   end
 
-  def init(options) do
-    # initialize options
-    options
-  end
 
   defp post_data(conn, channel_id) do
     store = Process.whereis(:store)
@@ -71,6 +76,18 @@ defmodule Hub do
     end
 
     send_resp(conn, 201, channel_id)
+  end
+
+  defp sub_data(conn, channel_id) do
+    store = Process.whereis(:store)
+
+    data =
+      case Store.get(store, channel_id) do
+        nil -> Store.put(store, channel_id, Channel.make()) |> Channel.sub()
+        channel -> Channel.sub(channel)
+      end
+
+    send_resp(conn, 200, data)
   end
 
   defp delete_channel(conn, channel_id) do
