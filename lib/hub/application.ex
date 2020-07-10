ffdb6e2b824b5827560df2940110acda9639f9a2
Roig <groig@protonmail.com>
2020-07-10 11:35:02 -0400

    get the port from the environment

--- lib/hub/application.ex
+++ lib/hub/application.ex
@@ -6,14 +6,27 @@ defmodule Hub.Application do
   use Application
 
   def start(_type, _args) do
+    port = System.get_env("PORT", "4000") |> String.to_integer()
+
     children = [
       # Starts a worker by calling: Hub.Worker.start_link(arg)
       # {Hub.Worker, arg}
-      {Plug.Cowboy, scheme: :http, plug: Hub, options: [port: 4000]}
+      {Plug.Cowboy, scheme: :http, plug: Hub, options: [port: port]}
     ]
 
-    :ets.new(:prodconchannels, [:public, :named_table, write_concurrency: true, read_concurrency: true])
-    :ets.new(:pubsubchannels, [:public, :named_table, write_concurrency: true, read_concurrency: true])
+    :ets.new(:prodconchannels, [
+      :public,
+      :named_table,
+      write_concurrency: true,
+      read_concurrency: true
+    ])
+
+    :ets.new(:pubsubchannels, [
+      :public,
+      :named_table,
+      write_concurrency: true,
+      read_concurrency: true
+    ])
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
